160b1c9bb80c8f5da670b83e108cd1dd2aae6fd2
nils <nils.christian@web.de>
2022-02-10 13:56:15 +0100

    improved overlay@workComponent

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -31,18 +31,18 @@ export class SkillsComponent implements OnInit {
     this.skills=[
       new Skill("assets/img/Group 205.png","HTML/CSS",
       
-      "My very first projects are plain HTML/CSS. check my gitHub nilsSpi for some static but neatly designed webpages like KochWelt and sakura Ramen. in the later projects I used SCSS and the framework bootstrap, i.e in the quizApp"),
+      " static but neatly designed webpages like KochWelt and sakura Ramen."),
       new Skill("assets/img/java script.png","JavaScript",
-      "example JS projects of mine in my gitHub: Join,El-Pollo-loco"),
+      "Join: project manegment tool,El-Pollo-loco: jump and run canvas game"),
       new Skill("assets/img/angular.png","Angular",
-      "example JS projects of mine in my gitHub: Ring of Fire,myPage"),
+      "Ring of fire: popular drinking game,myPage: code of this very site"),
       new Skill("assets/img/ts-logo-128.png","TypeScript",
-      "example JS projects of mine in my gitHub: Ring of Fire,myPage"),
+      "Ring of fire: popular drinking game,Ring of fire multiplayer with firebase,myPage: code of this very site"),
       new Skill("assets/img/Git-Icon-1788C 2.png","Git",
       "I love team projects!The version control is extreme useful. apropos Git... check my gitHub for my team projects like KochWelt and Join"),
       new Skill("assets/img/icons8-rest-api-80 2.png",
       "Rest APIs",
-      "example API projects of mine in my gitHub: Render the Saurus,pokedex"),
+      "example API projects of mine : Render the Saurus,pokedex"),
       new Skill("assets/img/octave-logo.png","Octave",
       "By studying mathematics i learned to work with octave and matlab. Especially numerical methods and optimization of non linear functions are beloved subjects of mine"),
     ];
